ad9ff2419340ee9bc32978b6fc5d4c680c7254bd
naokikimura <n.kimura.cap@gmail.com>
2013-03-05 00:16:32 +0900

    Refactoring

--- api.js
+++ api.js
@@ -1,153 +1,161 @@
 var common = {
     methods: [
         {
-            "name":"save"
-          , "return": { "type": "boolean" }
-          , "args": [
-                { "name": "name", "type": "string"}
-              , { "name": "id", "type": "string"}
+            "name": "save",
+            "return": { "type": "boolean" },
+            "args": [
+                { "name": "name", "type": "string"},
+                { "name": "id", "type": "string"}
             ]
-        }
-      , {
-            "name": "load"
-          , "return": { "type": "boolean" }
-          , "args": [
-                { "name": "name", "type": "string"}
-              , { "name": "id", "type": "string"}
-            ]
-        }
-      , {
-            "name": "get_config"
-          , "return": { "type": "string" }
-          , "args": [
-                { "name": "name", "type": "string"}
-            ]
-        }
-      , {
-            "name": "get_status"
-          , "return": { "type": "object" }
-          , "args": [
-                { "name": "name", "type": "string"}
-            ]
-        }
-    ]
-  , types: [
+        },
         {
-            "name": "datum"
-          , "type": "array"
-          , "items": [
+            "name": "load",
+            "return": { "type": "boolean" },
+            "args": [
+                { "name": "name", "type": "string"},
+                { "name": "id", "type": "string"}
+            ]
+        },
+        {
+            "name": "get_config",
+            "return": { "type": "string" },
+            "args": [
+                { "name": "name", "type": "string"}
+            ]
+        },
+        {
+            "name": "get_status",
+            "return": { "type": "object" },
+            "args": [
+                { "name": "name", "type": "string"}
+            ]
+        }
+    ],
+    types: [
+        {
+            "name": "datum",
+            "type": "array",
+            "items": [
                 {
-                    "name": "string_values"
-                  , "type": "array"
-                  , "items": {
-                        "type": "array"
-                      , "items": [
+                    "name": "string_values",
+                    "type": "array",
+                    "items": {
+                        "type": "array",
+                        "items": [
+                            { "type": "string" },
                             { "type": "string" }
-                          , { "type": "string" }
                         ]
                     }
-                }
-              , {
-                    "name": "num_values"
-                  , "type": "array"
-                  , "items": {
-                        "type": "array"
-                      , "items": [
-                            { "type": "string" }
-                          , { "type": "number" }
+                },
+                {
+                    "name": "num_values",
+                    "type": "array",
+                    "items": {
+                        "type": "array",
+                        "items": [
+                            { "type": "string" },
+                            { "type": "number" }
                         ]
                     }
                 }
             ]
         }
     ]
-}
+};
+
 module.exports = {
     Classifier: {
         methods: common.methods.concat([
-            'train'
-          , 'classify'])
-      , types: common.types.concat([
+            'train',
+            'classify'
+        ]),
+        types: common.types.concat([
             {
-                "name": "estimate_result"
-              , "type": "array"
-              , "items": [
+                "name": "estimate_result",
+                "type": "array",
+                "items": [
                     {
-                        "name":"label"
-                      , "type":"string"
-                    }
-                  , {
-                        "name":"score"
-                      , "type":"double"
+                        "name": "label",
+                        "type": "string"
+                    },
+                    {
+                        "name": "score",
+                        "type": "double"
                     }
                 ]
-            } ])
-    }
-  , Regression: {
+            }
+        ])
+    },
+    Regression: {
         methods: common.methods.concat([
-            'train'
-          , 'estimate'])
-      , types: common.types.concat([])
-    }
-  , Recommender: {
+            'train',
+            'estimate'
+        ]),
+        types: common.types.concat([])
+    },
+    Recommender: {
         methods: common.methods.concat([
-            'clear_row'
-          , 'update_row'
-          , 'clear'
-          , 'complete_row_from_id'
-          , 'complete_row_from_datum'
-          , 'similar_row_from_id'
-          , 'similar_row_from_datum'
-          , 'decode_row'
-          , 'get_all_rows'
-          , 'calc_similarity'
-          , 'calc_l2norm'])
-      , types: common.types.concat([
-            "similar_result"])
-    }
-  , Anomaly: {
+            'clear_row',
+            'update_row',
+            'clear',
+            'complete_row_from_id',
+            'complete_row_from_datum',
+            'similar_row_from_id',
+            'similar_row_from_datum',
+            'decode_row',
+            'get_all_rows',
+            'calc_similarity',
+            'calc_l2norm'
+        ]),
+        types: common.types.concat(["similar_result"])
+    },
+    Anomaly: {
         methods: common.methods.concat([
-            'clear_row'
-          , 'add'
-          , 'update'
-          , 'clear'
-          , 'calc_score'
-          , 'get_all_rows'])
-      , types: common.types.concat([])
-    }
-  , Stat: {
+            'clear_row',
+            'add',
+            'update',
+            'clear',
+            'calc_score',
+            'get_all_rows'
+        ]),
+        types: common.types.concat([])
+    },
+    Stat: {
         methods: common.methods.concat([
-            'push'
-          , 'sum'
-          , 'stddev'
-          , 'max'
-          , 'min'
-          , 'entropy'
-          , 'moment'])
-      , types: common.types.concat([])
-    }
-  , Graph: {
+            'push',
+            'sum',
+            'stddev',
+            'max',
+            'min',
+            'entropy',
+            'moment'
+        ]),
+        types: common.types.concat([])
+    },
+    Graph: {
         methods: common.methods.concat([
-            'create_node'
-          , 'remove_node'
-          , 'update_node'
-          , 'create_edge'
-          , 'update_edge'
-          , 'remove_edge'
-          , 'get_centrality'
-          , 'add_centrality_query'
-          , 'add_shortest_path_query'
-          , 'remove_centrality_query'
-          , 'remove_shortest_path_query'
-          , 'get_shortest_path'
-          , 'update_index'
-          , 'clear'
-          , 'get_node'
-          , 'get_edge'])
-      , types: common.types.concat([
-            "node"
-          , "preset_query"
-          , "edge"
-          , "shortest_path_query"])
+            'create_node',
+            'remove_node',
+            'update_node',
+            'create_edge',
+            'update_edge',
+            'remove_edge',
+            'get_centrality',
+            'add_centrality_query',
+            'add_shortest_path_query',
+            'remove_centrality_query',
+            'remove_shortest_path_query',
+            'get_shortest_path',
+            'update_index',
+            'clear',
+            'get_node',
+            'get_edge'
+        ]),
+        types: common.types.concat([
+            "node",
+            "preset_query",
+            "edge",
+            "shortest_path_query"
+        ])
     }
-}
+};
--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-var api = require('./api')
-  , rpc = require('./lib/msgpack-rpc')
+/*jslint forin: true, sloppy: true, passfail: false, node: true */
 
-var debug;
-if (process.env.NODE_DEBUG && /jubatus/.test(process.env.NODE_DEBUG)) {
-  debug = function(x) { console.error('JUBATUS:', x); };
-} else {
-  debug = function() { };
-}
+var api = require('./api'),
+    rpc = require('./lib/msgpack-rpc');
 
-for (var className in api) {
-    var package = {}
-    package[className] = createConstructor(className)
-    module.exports[className.toLowerCase()] = { client: package }
+var isDebugEnabled = process.env.NODE_DEBUG && (/jubatus/).test(process.env.NODE_DEBUG),
+    debug = isDebugEnabled ? function (x) { console.error('JUBATUS:', x); } : function () {};
+
+function toArray(args) {
+    return Array.prototype.slice.call(args);
 }
 
 function createConstructor(className) {
-    var constructor = function() {
-        if(!(this instanceof arguments.callee)) {
+    var constructor = function (portNumber, hostName) {
+        if (!(this instanceof constructor)) {
             throw new Error(className + ' is constructor.');
         }
 
-        var port = arguments[0] || 9199
-          , host = arguments[1] || 'localhost'
-          , client = rpc.createClient(port, host)
-        for(var propertyName in this) {
-            client[propertyName] = this[propertyName]
+        var port = portNumber || 9199,
+            host = hostName || 'localhost',
+            client = rpc.createClient(port, host),
+            propertyName;
+        for (propertyName in this) {
+            debug(propertyName);
+            client[propertyName] = this[propertyName];
         }
         return client;
-    }
-    api[className].methods.forEach(function(method) {
-        var methodName = method.name || method
-        constructor.prototype[methodName] = function() {
-            var params = toArray(arguments)
-              , hasCallback = (typeof params[params.length -1] === 'function')
-              , callback = hasCallback ? params.pop() : undefined
-            this.call(methodName, params, callback)
-        }
-    })
-    return constructor
+    };
+    api[className].methods.forEach(function (method) {
+        debug(method);
+        var methodName = method.name || method;
+        constructor.prototype[methodName] = function () {
+            var params = toArray(arguments),
+                hasCallback = (typeof params[params.length - 1] === 'function'),
+                callback = hasCallback ? params.pop() : undefined;
+            this.call(methodName, params, callback);
+        };
+    });
+    return constructor;
 }
 
-function toArray(args) {
-    var a = []
-    for(var i = 0; i < args.length; i++) {
-        a.push(args[i])
-    }
-    return a;
-}
+Object.keys(api).forEach(function (className) {
+    var client = {};
+    client[className] = createConstructor(className);
+    module.exports[className.toLowerCase()] = { client: client };
+});
