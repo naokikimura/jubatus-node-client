dfa3ac249a0bec31672473b8e9b189718784b59e
naokikimura <n.kimura.cap@gmail.com>
2018-02-15 11:59:22 +0900

    This commit fix #41 and fix #42

--- index.js
+++ index.js
@@ -24,10 +24,8 @@ function toOptions(args) {
     return { rpcClient, port, host, name, timeoutSeconds };
 }
 
-function createConstructor(className, schema) {
-    var constructor = function constructor(...args) {
-        if (!(this instanceof constructor)) { throw new Error(`${className} is constructor.`); }
-
+function createSuperConstructor() {
+    const constructor = function constructor(...args) {
         const options = toOptions(args);
         const { port = 9199, host = 'localhost', timeoutSeconds = 0, rpcClient } = options;
         let { name = '' } = options ;
@@ -37,10 +35,12 @@ function createConstructor(className, schema) {
         Object.defineProperty(this, 'client', {
             get() { return client; }
         });
+
         Object.defineProperty(this, 'name', {
             get() { return name; },
             set(value) { name = value; }
         });
+
         return this;
     };
 
@@ -59,37 +59,55 @@ function createConstructor(className, schema) {
         this.name = value;
     };
 
-    return schema.items.map(({ id: rpcName, properties: { 'arguments': argumentsSchema, 'return': returnSchema } }) => {
-        argumentsSchema.definitions = returnSchema.definitions = schema.definitions;
-        const validator = new jsonschema.Validator();
-        const methodName = toCamelCase(rpcName),
-            assertParams = isProduct ? () => {} : params => {
-                const result = validator.validate(params, argumentsSchema);
-                assert.ok(result.valid, util.format('%j', result.errors));
-            },
-            assertReturn = isProduct ? () => {} : returnValue => {
-                const result = validator.validate(returnValue, returnSchema);
-                assert.ok(result.valid, util.format('%j', result.errors));
+    return constructor;
+}
+
+function buildMethods(schema, constructor) {
+    return Object.keys(schema.properties)
+        .map(method => schema.properties[method])
+        .map(({ id: rpcName, properties: { 'arguments': argumentsSchema, 'return': returnSchema } }) => {
+            argumentsSchema.definitions = returnSchema.definitions = schema.definitions;
+            const validator = new jsonschema.Validator();
+            const methodName = toCamelCase(rpcName),
+                assertParams = isProduct ? () => {} : params => {
+                    const result = validator.validate(params, argumentsSchema);
+                    assert.ok(result.valid, util.format('%j', result.errors));
+                },
+                assertReturn = isProduct ? () => {} : returnValue => {
+                    const result = validator.validate(returnValue, returnSchema);
+                    assert.ok(result.valid, util.format('%j', result.errors));
+                };
+            return [ rpcName, methodName, assertParams, assertReturn ];
+        })
+        .reduce((constructor, [ rpcName, methodName, assertParams, assertReturn ]) => {
+            constructor.prototype[methodName] = function (...params) {
+                const self = this,
+                    client = self.getClient(),
+                    callback = (typeof params[params.length - 1] === 'function') && params.pop();
+                assertParams(params);
+                params.unshift(self.getName());
+                if (callback) {
+                    client.request.apply(client, [ rpcName ].concat(params, (error, result, msgid) => {
+                        if (!error) { assertReturn(result); }
+                        callback.call(self, error, result, msgid);
+                    }));
+                } else {
+                    return client.request.apply(client, [ rpcName ].concat(params));
+                }
             };
-        return [ rpcName, methodName, assertParams, assertReturn ];
-    }).reduce((constructor, [ rpcName, methodName, assertParams, assertReturn ]) => {
-        constructor.prototype[methodName] = function (...params) {
-            const self = this,
-                client = self.getClient(),
-                callback = (typeof params[params.length - 1] === 'function') && params.pop();
-            assertParams(params);
-            params.unshift(self.getName());
-            if (callback) {
-                client.request.apply(client, [ rpcName ].concat(params, (error, result, msgid) => {
-                    if (!error) { assertReturn(result); }
-                    callback.call(self, error, result, msgid);
-                }));
-            } else {
-                return client.request.apply(client, [ rpcName ].concat(params));
-            }
-        };
-        return constructor;
-    }, constructor);
+            return constructor;
+        }, constructor);
+}
+
+function createConstructor(className, schema, superConstructor) {
+    const constructor = function constructor(...args) {
+        assert(this instanceof constructor, `${className} is constructor.`);
+
+        superConstructor.apply(this, args);
+        return this;
+    };
+    util.inherits(constructor, superConstructor);
+    return buildMethods(schema, constructor);
 }
 
 const dirname = path.resolve(__dirname, './api/');
@@ -99,7 +117,7 @@ const services = fs.readdirSync(dirname)
     .filter(file => fs.statSync(file).isFile())
     .map(file => {
         const service = toCamelCase('_' + path.basename(file, '.json'));
-        const schema = JSON.parse(fs.readFileSync(file).toString());
+        const schema = JSON.parse(fs.readFileSync(file));
         return ({ [service]: schema });
     })
     .reduce((accumulator, current) => Object.assign(accumulator, current));
@@ -109,13 +127,14 @@ const schemas = Object.keys(services)
     .map(serviceName =>{
         const service = services[serviceName];
         service.definitions = Object.assign(service.definitions, common.definitions);
-        service.items = service.items.concat(common.items);
+        service.properties = Object.assign(service.properties, common.properties);
         return { [serviceName]: service };
     })
     .reduce((accumulator, current) => Object.assign(accumulator, current));
+const superConstructor = createSuperConstructor();
 Object.keys(schemas).forEach(function (className) {
     const schema = schemas[className];
     debug(className, schema);
-    const constructor = createConstructor(className, schema);
+    const constructor = createConstructor(className, schema, superConstructor);
     module.exports[className.toLowerCase()] = { client: { [className]: constructor } };
 });
